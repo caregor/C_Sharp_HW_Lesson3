52af304b0f9eb5b24688d43739cc805d2b9e32f0
Heavy <caregor@gmail.com>
2022-06-28 23:30:56 +0300

    Task_3 is done.

---
+++ Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 3
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+int Promt (string message)
+{
+    System.Console.Write(message);
+    int ret = int.Parse(Console.ReadLine());
+    return ret;
+}
+
+int Pow (int a, int b)
+{
+    int res = a;
+    for (int i=1; i < b; i++)
+    {
+        res = res * a;
+    }
+return res;
+}
+
+int number = Promt("Enter a number:");
+System.Console.Write($"{number} -> ");
+
+for(int i=1; i <= number; i++)
+{
+    System.Console.Write($"{Pow(i,3)} ");
+}
